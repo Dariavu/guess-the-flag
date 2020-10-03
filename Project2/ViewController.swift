5cf6f66fa537b5323e7c426f12feef2260670cbb
Dariya Vuyiv <vuyiv@gmail.com>
2020-10-03 16:48:22 +0300

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Daria Vuyiv on 10/3/20.
+//  Copyright © 2020 Daria Vuyiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
